142f602df31bcfafc85e23003ffa3aa1a3644fdd
abraidotti <alessandro.braidotti@gmail.com>
2019-07-16 10:27:45 -0400

    add acceptance tests

--- tests/steps/init.js
+++ tests/steps/init.js
@@ -15,7 +15,8 @@ let init = co.wrap(function* () {
   process.env.restaurants_table = "restaurants";
   process.env.AWS_REGION = "us-east-1";
   process.env.cognito_client_id = "test_cognito_client_id";
-  process.env.cognito_user_pool_id = "test_cognito_user_pool_id";
+  process.env.cognito_user_pool_id = "us-east-1_wH7VM1PPz";
+  process.env.cognito_server_client_id = "3ur2m4jas511oq1ot1cgsa648f";
 
   let cred = (yield awscred.loadAsync()).credentials;
 
